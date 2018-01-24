51c5fc646059ac63e4eda6836f365c5f565b01dd
Anton Nagy <nagya@carleton.edu>
2018-01-24 17:26:30 -0600

    Made check answer function and jazz like that

--- scripts/test_viewController.js
+++ scripts/test_viewController.js
@@ -1,5 +1,11 @@
+var currentButton = "a";
+var answered = false;
+
 function getQuestionClick() {
+    document.getElementById(currentButton + '-div').innerHTML = '<button id="' + currentButton + `-button" class="btn btn-primary" onclick="checkAnswer('` + currentButton + `');">` + currentButton[0].toUpperCase() + `</button><span id="` + currentButton + `">` + document.getElementById(currentButton).innerHTML + `</span>`;
+
     difficulty = "easy";
+    answered = false;
     getOneMCQuestion(difficulty);
 }
 
@@ -10,4 +16,17 @@ function displayQuestion(question, answers) {
     $("#c").text(answers[2]);
     $("#d").text(answers[3]);
     console.log(difficulty);
+}
+
+function checkAnswer(answer) {
+    if (!answered) {
+        correct = 'a';
+        currentButton = answer;
+        if (answer == correct) {
+            $('#' + answer + '-button').css('backgroundColor','green');
+        } else {
+            $('#' + answer + '-button').css('backgroundColor','red');
+        }
+        answered = true;
+    }
 }
