bdb827de725019dcf6c620c2fae15b6f9ea20143
noah-someck <jamessomeck@yahoo.com>
2018-01-24 15:32:12 -0600

    Adding UI stuff and setting up home page

---
+++ scripts/localController.js
@@ -0,0 +1,67 @@
+function clickContinue() {
+    "use strict";
+    var playerOne = "Player 1";
+    var playerTwo = "Player 2";
+    
+    var playerOneInput = document.getElementById("player-one-input").value;
+    var playerTwoInput = document.getElementById("player-two-input").value;
+    
+    if (playerOneInput != "") {
+        playerOne = playerOneInput;
+    }
+    if (playerTwoInput != "") {
+        playerTwo = playerTwoInput;
+    }
+    
+    document.getElementById("local-content").innerHTML = `
+        <div class="jumbotron">
+            <h1>BookWorm Trivia</h1>
+        </div>
+        
+        <div class="container" style="font-size:20px;">
+            <b>` + playerOne + `:</b>
+            <b style="margin-right:10px;">0</b>
+            <b>` + playerTwo+ `:</b>
+            <b>0</b>
+        </div>
+        <br>
+
+        <div class="container">
+            <button class="btn btn-primary" onclick="getQuestionClick();">Get Questions</button>
+        </div>
+        <br/>
+        <div class="container">
+            <h2 id="question">Question</h2>
+            <br/>
+            <div class="container">
+                <button class="btn btn-primary">A</button><span id="a"> Answer A</span>
+            </div>
+            <br/>
+            <div class="container">
+                <button class="btn btn-primary">B</button><span id="b"> Answer B</span>
+            </div>
+            <br/>
+            <div class="container">
+                <button class="btn btn-primary">C</button><span id="c"> Answer C</span>
+            </div>
+            <br/>
+            <div class="container">
+                <button class="btn btn-primary">D</button><span id="d"> Answer D</span>
+            </div>
+            <br/>
+        </div>`;
+}
+
+function getQuestionClick() {
+    difficulty = "easy";
+    getOneMCQuestion(difficulty);
+}
+
+function displayQuestion(question, answers) {
+    $("#question").text(question);
+    $("#a").text(answers[0]);
+    $("#b").text(answers[1]);
+    $("#c").text(answers[2]);
+    $("#d").text(answers[3]);
+    console.log(difficulty);
+}
---
+++ scripts/onlineController.js
@@ -0,0 +1,9 @@
+function clickPrivateRoom() {
+    "use strict";
+    document.getElementById("set-online").innerHTML = `
+        <p class="h2" style="margin-bottom:20px;">Enter Room Name:</p>
+        <input class="form-control-feedback" style="height:30px; width:280px; font-size:16px; padding:5px; border:2px solid #0C60FE">
+        <br>
+        <br>
+        <button style="width:250px;" class="btn btn-primary">Continue</button>`;
+}
