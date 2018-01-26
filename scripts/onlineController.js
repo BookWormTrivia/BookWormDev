7aeb7ca96f739b4da7291cf656f43c445eccc950
noah-someck <jamessomeck@yahoo.com>
2018-01-25 20:47:34 -0600

    More CSS moving

--- scripts/onlineController.js
+++ scripts/onlineController.js
@@ -2,8 +2,8 @@ function clickPrivateRoom() {
     "use strict";
     document.getElementById("set-online").innerHTML = `
         <p class="h2" style="margin-bottom:20px;">Enter Room Name:</p>
-        <input class="form-control-feedback" style="height:30px; width:280px; font-size:16px; padding:5px; border:2px solid #0C60FE" placeholder="Room Name">
+        <input id="room-input" placeholder="Room Name">
         <br>
         <br>
-        <button style="width:250px;" class="btn btn-primary">Continue</button>`;
+        <button class="btn btn-primary">Continue</button>`;
 }
