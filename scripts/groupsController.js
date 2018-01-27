92949b73b09cf31fa6320e18d98fe0d81cf0131f
Aidan White <aidan.white36@gmail.com>
2018-01-27 17:58:18 -0600

    Create page for groups

---
+++ scripts/groupsController.js
@@ -0,0 +1,43 @@
+function getGroupQuestionClick() {
+    var group = "1";
+    getGroupQuestions(group);
+}
+
+function displayQuestion(question, answers) {
+    console.log(answers[3]);
+    correct = answers[3];
+    for (var i = answers.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = answers[i];
+        answers[i] = answers[j];
+        answers[j] = temp;
+    }
+    for (var i = answers.length - 1; i >= 0; i--) {
+        if (correct == answers[i]) {
+            console.log(correct);
+            console.log(i);
+            if (i==1) {
+                correct = 'b';
+            } else if (i==2) {
+                correct = 'c';
+            } else if (i==3) {
+                correct = 'd';
+            } else {
+                correct = 'a';
+            }
+        }
+        console.log(correct);
+    }
+
+    $("#question").text(question);
+    $("#a").text(answers[0]);
+    $("#b").text(answers[1]);
+    $("#c").text(answers[2]);
+    $("#d").text(answers[3]);
+    console.log(difficulty);
+}
+
+function toggleCreateGroup() {
+    $("#play_with_group").toggle();
+    $("#create_group").fadeToggle();
+}
